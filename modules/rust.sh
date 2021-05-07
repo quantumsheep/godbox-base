f22e71ea69c75e777bfe5f3454e1f2e5925ff002
quantumsheep <nathanael.dmc@outlook.fr>
2021-05-08 01:04:45 +0200

    Remove "\" in rust.sh

--- modules/rust.sh
+++ modules/rust.sh
@@ -4,12 +4,12 @@ set -xe
 
 export RUST_VERSIONS="1.52.0 beta nightly"
 
-for VERSION in $RUST_VERSIONS; do \
+for VERSION in $RUST_VERSIONS; do
     curl -fSsL "https://static.rust-lang.org/dist/rust-$VERSION-x86_64-unknown-linux-gnu.tar.gz" -o /tmp/rust-$VERSION.tar.gz
     mkdir /tmp/rust-$VERSION
     tar -xf /tmp/rust-$VERSION.tar.gz -C /tmp/rust-$VERSION --strip-components=1
     rm /tmp/rust-$VERSION.tar.gz
     cd /tmp/rust-$VERSION
     ./install.sh --prefix=/usr/local/rust-$VERSION --components=rustc,rust-std-x86_64-unknown-linux-gnu,cargo
-    rm -rf /tmp/*; \
+    rm -rf /tmp/*;
 done
